5e1f89295b3567ec5f113498f2d194f969728506
user1 <user01@em/com>
2025-05-12 21:42:51 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,59 @@
+let add_btn = document.getElementById("add-btn")
+let task = document.getElementById("task")
+let tasks_list = document.getElementById("tasks-list")
+let err_msg = document.getElementById("err-msg")
+
+
+// get the data
+fetch('https://682199fa259dad2655afc100.mockapi.io/tasks')
+      .then(response => response.json())
+      .then(data => {
+
+        data.forEach(element => {
+            let new_task = document.createElement("li")
+            new_task.innerText = element.text
+
+            new_task.classList.add("list-group-item");
+            new_task.classList.add("my-1");
+            new_task.classList.add("rounded");
+            new_task.classList.add("d-flex");
+            new_task.classList.add("align-items-center");
+            new_task.classList.add("justify-content-between");
+
+            let del_btn = document.createElement("button")
+            del_btn.innerText = "X"
+            del_btn.classList.add("btn")
+            del_btn.classList.add("btn-danger")
+
+            new_task.appendChild(del_btn)
+            tasks_list.appendChild(new_task)
+        });
+})
+
+
+// add new task      
+add_btn.addEventListener("click", () => {
+
+    if(task.value.length == "") {
+        err_msg.innerText = "task must not be empty"
+        return
+    } else {
+        err_msg.innerText = ""
+    }
+
+
+    // post a new task
+    fetch('https://682199fa259dad2655afc100.mockapi.io/tasks', {
+        method: 'POST',
+        body: JSON.stringify({
+            text: task.value,
+        }),
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+        },
+        })
+        .then((response) => response.json())
+        .then(() => {
+            location.reload();
+    });
+})

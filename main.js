e27a14645c5caf386011a24746bb530ff8f82832
user1 <user01@em/com>
2025-05-12 22:37:40 +0300

    final v1

--- main.js
+++ main.js
@@ -19,13 +19,37 @@ fetch('https://682199fa259dad2655afc100.mockapi.io/tasks')
             new_task.classList.add("d-flex");
             new_task.classList.add("align-items-center");
             new_task.classList.add("justify-content-between");
+            new_task.classList.add("flex-wrap");
+            
 
             let del_btn = document.createElement("button")
             del_btn.innerText = "X"
             del_btn.classList.add("btn")
             del_btn.classList.add("btn-danger")
 
-            new_task.appendChild(del_btn)
+            // delete a task
+            del_btn.addEventListener("click", () => {
+                fetch(`https://682199fa259dad2655afc100.mockapi.io/tasks/${element.id}`, {
+                    method: 'DELETE',
+                }).then(() => {
+                    location.reload();
+                })
+            })
+
+            let span = document.createElement("span")
+            span.innerText = element.time
+            span.style.color = "grey"
+            span.classList.add("px-3");
+
+            let date_box = document.createElement("div")
+            date_box.appendChild(span)
+            date_box.appendChild(del_btn)
+            date_box.classList.add("d-flex");
+            date_box.classList.add("align-items-center");
+
+            new_task.appendChild(date_box)
+            // new_task.appendChild(span)
+            // new_task.appendChild(del_btn)
             tasks_list.appendChild(new_task)
         });
 })
@@ -41,12 +65,15 @@ add_btn.addEventListener("click", () => {
         err_msg.innerText = ""
     }
 
-
+    let date = new Date()
+    let date_text = `${date.getFullYear()}/${date.getMonth()+1}/${date.getDate()}, ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`
+    
     // post a new task
     fetch('https://682199fa259dad2655afc100.mockapi.io/tasks', {
         method: 'POST',
         body: JSON.stringify({
             text: task.value,
+            time: date_text
         }),
         headers: {
             'Content-type': 'application/json; charset=UTF-8',
